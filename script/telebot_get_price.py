caa30ffe669a365533d67d59e760c401611fc418
Ho Minh Tri <trihm23@fpt.com.vn>
2023-05-02 17:06:12 +0700

    rewrite as register function

--- script/telebot_get_price.py
+++ script/telebot_get_price.py
@@ -1,7 +1,6 @@
 import ccxt
 import telebot
 import json
-from telebot import types
 
 # Define a list of pre-defined coin pairs
 coin_pairs = ['BTC/USDT', 'ETH/USDT', 'ADA/USDT']
@@ -22,33 +21,32 @@ exchange = ccxt.binance()
 # Define the function to handle the '/price' command
 @bot.message_handler(commands=['price'])
 def send_price(message):
-    # Create a message with available coin pairs as options
-    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-    buttons = [types.KeyboardButton(pair) for pair in coin_pairs]
-    markup.add(*buttons)
+    # Create a keyboard with available coin pairs as options
+    keyboard = telebot.types.ReplyKeyboardMarkup(row_width=1)
+    for pair in coin_pairs:
+        keyboard.add(telebot.types.KeyboardButton(pair))
+        
+    # Send a message with available coin pairs as options
     response = "Please choose a coin pair:"
-    
-    # Send the message with available coin pairs as buttons
-    bot.send_message(message.chat.id, response, reply_markup=markup)
+    bot.send_message(message.chat.id, response, reply_markup=keyboard)
+
+    # Set the next expected message to be the chosen coin pair
+    bot.register_next_step_handler(message, get_price_coin_pair_choice)
 
 # Define the function to process the chosen coin pair
-@bot.message_handler(func=lambda message: True)
-def process_coin_pair_choice(message):
-    if message.text in coin_pairs:
-        try:
-            coin_pair = message.text
-            
-            # Fetch the ticker data for the coin pair
-            ticker = exchange.fetch_ticker(coin_pair)
-            
-            # Extract the price from the ticker data
-            price = ticker['last']
-            
-            # Send the price back to the user
-            response = f"Current price of {coin_pair}: {price}"
-        except:
-            response = "Error: Please choose a valid coin pair from the options."
-    else:
+def get_price_coin_pair_choice(message):
+    try:
+        coin_pair = message.text
+        
+        # Fetch the ticker data for the coin pair
+        ticker = exchange.fetch_ticker(coin_pair)
+        
+        # Extract the price from the ticker data
+        price = ticker['last']
+        
+        # Send the price back to the user
+        response = f"Current price of {coin_pair}: {price}"
+    except:
         response = "Error: Please choose a valid coin pair from the options."
         
     bot.send_message(message.chat.id, response)
